e06e8e3575b7c4a718598e56287f7f4275233d9e
saisilinus <saisilinus19@gmail.com>
2023-05-25 16:52:31 +0300

    fix: mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -23,19 +23,22 @@ function App() {
   }
 
   return (
-    <div className='container flex flex-col justify-center items-center min-h-screen'>
-      <h2 className='text-lg font-bold mb-5'>This is an Image Framing App. Upload your image and it will be framed using the peaceJam custom frame</h2>
+    <div className='container flex flex-col justify-center items-center min-h-screen px-5'>
+      <h2 className='text-lg font-bold mb-5 mt-5'>This is an Image Framing App. Upload your image and it will be framed using the peaceJam custom frame</h2>
       <input type='file' name='uploadedImage' onChange={(e) => setUploadedImage(e.target.files[0])} className="mb-5" />
       {uploadedImage && image && (
-        <div className='flex justify-center gap-10 items-center'>
+        <div className='flex flex-col justify-center gap-10 items-center mb-5 md:flex-row'>
           <div className='flex flex-col text-center'>
-            <img src={URL.createObjectURL(uploadedImage)} className="h-96 w-auto rounded" alt='uploaded image' />
+            <img src={URL.createObjectURL(uploadedImage)} className="w-auto rounded md:h-96" alt='uploaded image' />
           </div>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hidden md:block">
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
           </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 block md:hidden">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+          </svg>
           <a href={image} download="image.png" className='flex flex-col items-center'>
-            <img src={image} className="h-96 w-96 mb-3" />
+            <img src={image} className="mb-3 md:h-96" />
             <p className='text-base text-center border border-black text-blue-700 px-3 py-1 rounded hover:bg-black hover:text-white max-w-fit'>Download</p>
           </a>
         </div>
