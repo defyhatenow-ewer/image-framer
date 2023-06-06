639abc8138c71620185caa0a6ec7eae60d862b6b
Dan King'ori <kingndiritu@gmail.com>
2023-06-06 12:37:06 +0300

    Update index.js
    2023 frame

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,6 @@ import mouth from './mouth.png';
 import fancyFrame from './frame-fancy.png';
 import frame from './frame.jpg';
 import ovalFrame from './wreath-4602807_1280.png';
-import peaceJamFrame from './peacejam2022frame-1.png';
+import peaceJamFrame from './PeaceJam2023_Facebook_Frame.png';
 
 export default { body, eyes, mouth, fancyFrame, frame, ovalFrame, peaceJamFrame };
