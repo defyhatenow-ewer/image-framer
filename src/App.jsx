aea7dc9e550588ee9d0a8c042e3b593a536c3777
saisilinus <saisilinus19@gmail.com>
2023-06-07 12:36:38 +0300

    refactor: icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import mergeImages from 'merge-images';
 import resizeFile from './resize';
 import assets from './assets';
+import { Spinner, ArrowDown, ArrowUp, Upload } from './Icons';
 
 function App() {
   const [englishImage, setEnglishImage] = useState(null);
@@ -44,19 +45,13 @@ function App() {
 
       {/* English frame */}
       <div className='flex flex-col items-center gap-5 mb-5 mt-5'>
-        {!englishImage ? <img src={assets.peaceJam2023EN} alt="English frame"className='mb-3 md:h-96 md:w-96' /> : !englishMergedImage ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 animate-spin">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-        </svg> : (
+        {!englishImage ? <img src={assets.peaceJam2023EN} alt="English frame"className='mb-3 md:h-96 md:w-96' /> : !englishMergedImage ? <Spinner /> : (
           <div className='flex flex-col justify-center gap-10 items-center mb-5 md:flex-row'>
             <div className='flex flex-col text-center'>
               <img src={URL.createObjectURL(englishImage)} className="w-auto rounded md:h-96" alt='uploaded image' />
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hidden md:block">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 block md:hidden">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
-            </svg>
+            <ArrowDown />
+            <ArrowUp />
             <a href={englishMergedImage} download="image.png" className='flex flex-col items-center'>
               <img src={englishMergedImage} className="mb-3 md:h-96" />
               <p className='text-base text-center border border-black text-black px-3 py-1 rounded bg-yellow-300 hover:bg-black hover:text-white max-w-fit'>Download</p>
@@ -67,9 +62,7 @@ function App() {
         <label htmlFor='uploadFile' className='relative h-16 w-64 mb-5 rounded-2xl cursor-pointer md:w-96' style={{ backgroundColor: '#f6ff21' }}>
           <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center px-5'>
             <p className='text-center pe-2'>Click or drag your photo here to upload</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-            </svg>
+            <Upload />
           </div>
           <input id='uploadFile' type='file' name='uploadedImage' onChange={(e) => {
             setEnglishImage(e.target.files[0]);
@@ -79,19 +72,13 @@ function App() {
 
       {/* French frame */}
       <div className='flex flex-col items-center gap-5 mb-5 mt-5'>
-        {!frenchImage ? <img src={assets.peaceJam2023FR} alt="English frame"className='mb-3 md:h-96 md:w-96' /> : !frenchMergedImage ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 animate-spin">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-        </svg> : (
+        {!frenchImage ? <img src={assets.peaceJam2023FR} alt="English frame"className='mb-3 md:h-96 md:w-96' /> : !frenchMergedImage ? <Spinner /> : (
           <div className='flex flex-col justify-center gap-10 items-center mb-5 md:flex-row'>
             <div className='flex flex-col text-center'>
               <img src={URL.createObjectURL(frenchImage)} className="w-auto rounded md:h-96" alt='uploaded image' />
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hidden md:block">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 block md:hidden">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
-            </svg>
+            <ArrowDown />
+            <ArrowUp />
             <a href={frenchMergedImage} download="image.png" className='flex flex-col items-center'>
               <img src={frenchMergedImage} className="mb-3 md:h-96" />
               <p className='text-base text-center border border-black text-blue-700 px-3 py-1 rounded hover:bg-black hover:text-white max-w-fit'>Télécharger</p>
@@ -102,9 +89,7 @@ function App() {
         <label htmlFor='uploadFile' className='relative h-16 w-64 mb-5 rounded-2xl cursor-pointer md:w-96' style={{ backgroundColor: '#f6ff21' }}>
           <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center px-5'>
             <p className='text-center pe-2'>Cliquez ou faites glisser votre photo ici pour la télécharger</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-            </svg>
+            <Upload />
           </div>
           <input id='uploadFile' type='file' name='uploadedImage' onChange={(e) => {
             setfrenchImage(e.target.files[0]);
---
+++ src/Icons.jsx
@@ -0,0 +1,15 @@
+export const Spinner = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 animate-spin">
+<path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+
+export const ArrowDown = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hidden md:block">
+<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+</svg>
+
+export const ArrowUp = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 block md:hidden">
+<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+</svg>
+
+export const Upload = () => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+</svg>
