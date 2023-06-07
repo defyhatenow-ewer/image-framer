bc453c40177b2869c7818596e911e97db156adcc
saisilinus <saisilinus19@gmail.com>
2023-06-07 13:53:11 +0300

    feat: update frame links

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import fancyFrame from './frame-fancy.png';
 import frame from './frame.jpg';
 import ovalFrame from './wreath-4602807_1280.png';
 import peaceJamFrame from './PeaceJam2023_Facebook_Frame_en.png';
-import peaceJam2023EN from './PeaceJam2023_Facebook_Frame_french.png';
-import peaceJam2023FR from './peacejam2023frame-fr.png';
+import peaceJam2023EN from './PeaceJam2023_Facebook_Frame_en.png';
+import peaceJam2023FR from './PeaceJam2023_Facebook_Frame_french.png';
 
 export default { body, eyes, mouth, fancyFrame, frame, ovalFrame, peaceJamFrame, peaceJam2023EN, peaceJam2023FR };
